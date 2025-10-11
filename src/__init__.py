421e5908e1eb700545457f0ad94f3ef34df64dd0
eran <eran@lawpro.ai>
2025-10-11 21:23:42 +0300

    fix: avoid importing Trainer in package __init__ to prevent test import errors

--- src/__init__.py
+++ src/__init__.py
@@ -9,8 +9,7 @@ License: Apache 2.0
 """
 
 from src.models import PoHParser, BaselineParser, ParserBase
-from src.training.trainer import Trainer
 
 __version__ = "0.1.0"
 __author__ = "Eran Ben Artzy"
-__all__ = ["PoHParser", "BaselineParser", "ParserBase", "Trainer"]
+__all__ = ["PoHParser", "BaselineParser", "ParserBase"]
