3b67b596e3338359882a3e97e266ae5eee14903c
Eran Ben Artzy <eranbt92@gmail.com>
2026-01-01 20:17:21 +0200

    Fix duplicate --download argument in blocksworld_ppo_benchmark.py

--- experiments/blocksworld_ppo_benchmark.py
+++ experiments/blocksworld_ppo_benchmark.py
@@ -183,8 +183,6 @@ def parse_args():
                        help='Output directory for results')
     parser.add_argument('--seed', type=int, default=42,
                        help='Random seed')
-    parser.add_argument('--download', action='store_true',
-                       help='Download dataset if not present')
     
     return parser.parse_args()
 
